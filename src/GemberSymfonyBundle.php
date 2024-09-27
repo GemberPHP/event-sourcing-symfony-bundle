365d9f385736f55e33c4e6d60f9b42775279e3ba
Jeroen de Graaf <gember@jero.work>
2024-09-27 09:19:50 +0200

    Create Bundle file

---
+++ src/GemberSymfonyBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Gember\SymfonyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+final class GemberSymfonyBundle extends AbstractBundle
+{
+}
