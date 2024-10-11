23f61fd8ddf19529001159e9deeaea87d3e19413
Jeroen de Graaf <gember@jero.work>
2024-10-11 17:31:12 +0200

    Add missing symfony/property-access to serialize objects

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -14,4 +14,5 @@ return $config
     ->ignoreErrorsOnPackage('gember/identity-generator-symfony', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('gember/message-bus-symfony', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('gember/rdbms-event-store-doctrine-dbal', [ErrorType::UNUSED_DEPENDENCY])
-    ->ignoreErrorsOnPackage('gember/serializer-symfony', [ErrorType::UNUSED_DEPENDENCY]);
+    ->ignoreErrorsOnPackage('gember/serializer-symfony', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/property-access', [ErrorType::UNUSED_DEPENDENCY]);
