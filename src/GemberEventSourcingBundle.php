ed2b5bd3e669382f714cdcf90f3723434a05a551
Jeroen de Graaf <gember@jero.work>
2025-09-10 21:17:57 +0200

    Define services for domain command resolver

--- src/GemberEventSourcingBundle.php
+++ src/GemberEventSourcingBundle.php
@@ -118,6 +118,9 @@ final class GemberEventSourcingBundle extends AbstractBundle
             $services->get('gember.event_sourcing.resolver.domain_event.cached.cached_domain_event_resolver_decorator')
                 ->decorate('gember.event_sourcing.resolver.domain_event.domain_event_resolver');
 
+            $services->get('gember.event_sourcing.resolver.domain_command.cached.cached_domain_command_resolver_decorator')
+                ->decorate('gember.event_sourcing.resolver.domain_command.domain_command_resolver');
+
             $cacheType = isset($config['cache']['psr16']) ? 'psr16' : 'psr6';
             $cacheService = ltrim($config['cache'][$cacheType]['service'] ?? 'cache.app', '@');
 
