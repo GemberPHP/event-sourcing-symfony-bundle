7659fec8e51f4d9a8c4bda2fe06cda2a969fd247
Jeroen de Graaf <gember@jero.work>
2025-10-10 15:20:39 +0200

    Remove link to dependency-contracts - not directly needed

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -8,7 +8,6 @@ use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 $config = new Configuration();
 
 return $config
-    ->ignoreErrorsOnPackage('gember/dependency-contracts', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('gember/identity-generator-symfony', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('gember/message-bus-symfony', [ErrorType::UNUSED_DEPENDENCY])
     ->ignoreErrorsOnPackage('gember/rdbms-event-store-doctrine-dbal', [ErrorType::UNUSED_DEPENDENCY])
